251fe4cadcd099814aa43f00bfb5e20c83801f50
Park Gyeong Ho <59360980+kayheypark@users.noreply.github.com>
2020-12-03 23:35:36 +0900

    scss 1200

--- Views/Layout/Style.php
+++ Views/Layout/Style.php
@@ -1,4 +1,5 @@
 <link rel="stylesheet" href="/Content/node_modules/bootstrap/dist/css/bootstrap.css" />
+<link rel="stylesheet" href="/Content/node_modules/bootstrap/scss/main.css" />
 <script src="https://kit.fontawesome.com/44905f436c.js" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="/Style/Base.css" />
 <link rel="stylesheet" href="/Style/Default.css" />
