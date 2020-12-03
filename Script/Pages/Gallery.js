253375a5ba78d9f8c1c21e1e612696eb9f07bc06
Park Gyeong Ho <59360980+kayheypark@users.noreply.github.com>
2020-12-03 22:48:19 +0900

    슬랙

--- Script/Pages/Gallery.js
+++ Script/Pages/Gallery.js
@@ -1,16 +1,13 @@
-﻿function fnPageInit(){
-
-<!-- Initialize Swiper -->
-var swiper = new Swiper('.swiper-container', {
+﻿function fnPageInit() {
+  //Initialize Swiper
+  var swiper = new Swiper('.swiper-container', {
     slidesPerView: 4,
     centeredSlides: true,
     spaceBetween: 30,
     grabCursor: true,
     pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
+      el: '.swiper-pagination',
+      clickable: true,
     },
-});
-
-};
-
+  });
+}
